951d412c5a97bd885959a1a57a1ae0a09b11f061
Fernando Oliveira <primoflro@gmail.com>
2022-09-16 19:47:55 -0300

    Criacao dos testes para a página de Feedback

--- src/tests/Feeedback.spec.js
+++ src/tests/Feeedback.spec.js
@@ -1,69 +1,52 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import App from '../App';
 
 describe('Testing the Feedback.jsx page', () => {
-  it('test if the component exists', () => {
-    renderWithRouterAndRedux(<App />);
-    const PLAYER_USER = 'player';
-    const PLAYER_EMAIL = 'player@test.com';
-    const labelUser = screen.getByLabelText(USERNAME);
-    const labelEmail = screen.getByLabelText(EMAIL);
-
-  });
-
-
-  it('Testando se é possivel interagir com os inputs', () => {
-    renderWithRouterAndRedux(<Login />);
-
-    const USERNAME = 'Username';
-    const EMAIL = 'Email';
-    const CONTENT_EMAIL = 'dinhoouropreto@email.com';
-    const CONTENT_USERNAME = 'Dinho Ouro Preto';
-
-    const labelUser = screen.getByPlaceholderText(USERNAME);
-    const labelEmail = screen.getByPlaceholderText(EMAIL);
-
-    userEvent.type(labelUser, CONTENT_USERNAME);
-    userEvent.type(labelEmail, CONTENT_EMAIL);
-
-    expect(labelUser.value).toEqual(CONTENT_USERNAME);
-    expect(labelEmail.value).toEqual(CONTENT_EMAIL);
-  });
-
-  it('Testando o link do botão \'Play\'', () => {
-    const { history } = renderWithRouterAndRedux(<Login />);
-
-    const USERNAME = 'Username';
-    const EMAIL = 'Email';
-    const CONTENT_EMAIL = 'dinhoouropreto@email.com';
-    const CONTENT_USERNAME = 'Dinho Ouro Preto';
-
-    const labelUser = screen.getByPlaceholderText(USERNAME);
-    const labelEmail = screen.getByPlaceholderText(EMAIL);
-
-    userEvent.type(labelUser, CONTENT_USERNAME);
-    userEvent.type(labelEmail, CONTENT_EMAIL);
-
-    expect(labelUser.value).toEqual(CONTENT_USERNAME);
-    expect(labelEmail.value).toEqual(CONTENT_EMAIL);
-
-    const btnPlay = screen.getByTestId('btn-play');
-    userEvent.click(btnPlay);
+    const GOOD_RESULT = {
+        player: {
+            answers: 3,
+            email: 'test@email.com',
+            name: 'player',
+            assertions: 5,
+            trueAnswers: 2,
+            score: 200,
+          }
+    }
+    const BAD_RESULT = {
+        player: {
+            answers: 0,
+            email: 'test@email.com',
+            name: 'player',
+            assertions: 1,
+            trueAnswers: 1,
+            score: 23,
+          }
+    }
+  it('test if the component exists and appear all the infos of the game', () => {
+    const { history } = renderWithRouterAndRedux(<App />, GOOD_RESULT, '/feedback');
     const { location: { pathname } } = history;
-
-    expect(pathname).toEqual('/game');
+    expect(pathname).toEqual('/feedback');
+    const SCORE_FEEDBACK = screen.getByTestId('feedback-total-score');
+    const SCORE_TEXT = screen.getByTestId('feedback-text');
+    const SCORE_QUESTION = screen.getByTestId('feedback-total-question');
+    expect(SCORE_FEEDBACK.innerHTML).toBe('200');
+    expect(SCORE_TEXT.innerHTML).toBe('Well Done!');
+    expect(SCORE_QUESTION.innerHTML).toBe('5');
   });
-
-  it('Testando o link do botão \'Settings\'', () => {
-    const { history } = renderWithRouterAndRedux(<Login />);
-
-    const btnSettings = screen.getByTestId('btn-settings');
-    userEvent.click(btnSettings);
+  it('test if with a bad result change the text to Could Be Better!', () => {
+    renderWithRouterAndRedux(<App />, BAD_RESULT, '/feedback');
+    const SCORE_FEEDBACK = screen.getByTestId('feedback-text');
+    expect(SCORE_FEEDBACK.innerHTML).toBe('Could be better...');
+  })
+  it('test if the button play again works', () => {
+    const { history } = renderWithRouterAndRedux(<App />, BAD_RESULT, '/feedback');
+    const PLAY_AGAIN_BTN = screen.getByTestId('btn-play-again');
+    userEvent.click(PLAY_AGAIN_BTN);
     const { location: { pathname } } = history;
+    expect(pathname).toEqual('/');
 
-    expect(pathname).toEqual('/config');
   });
 });
