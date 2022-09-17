5d6b3eba7e716ba5376504fd5c740a80aef66784
Fernando Oliveira <primoflro@gmail.com>
2022-09-16 21:40:18 -0300

    Teste 1, se o component existe e tem as informações do titulo, do botao e dos jogadores ranqueados da forma certa

--- src/tests/Ranking.spec.js
+++ src/tests/Ranking.spec.js
@@ -4,48 +4,29 @@ import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
 import App from '../App';
 
-describe('Testing the Feedback.jsx page', () => {
-    const GOOD_RESULT = {
-        player: {
-            answers: 3,
-            email: 'test@email.com',
+describe('Testing the Ranking.jsx page', () => {
+    const INITIAL_STATE = {
+        ranking: {
+            players: [{
             name: 'player',
-            assertions: 5,
-            trueAnswers: 2,
-            score: 200,
-          }
+            assertions: 3,
+            score: 75,
+            hash:'5e9ce47004afb4ea3db4a123dadb4cb8',
+          }],
+        }
     }
-    const BAD_RESULT = {
-        player: {
-            answers: 0,
-            email: 'test@email.com',
-            name: 'player',
-            assertions: 1,
-            trueAnswers: 1,
-            score: 23,
-          }
-    }
-  it('test if the component exists and appear all the infos of the game', () => {
-    const { history } = renderWithRouterAndRedux(<App />, GOOD_RESULT, '/feedback');
-    const { location: { pathname } } = history;
-    expect(pathname).toEqual('/feedback');
-    const SCORE_FEEDBACK = screen.getByTestId('feedback-total-score');
-    const SCORE_TEXT = screen.getByTestId('feedback-text');
-    const SCORE_QUESTION = screen.getByTestId('feedback-total-question');
-    expect(SCORE_FEEDBACK.innerHTML).toBe('200');
-    expect(SCORE_TEXT.innerHTML).toBe('Well Done!');
-    expect(SCORE_QUESTION.innerHTML).toBe('5');
+  it('test if the component exists and have the header, button to home and the player rank', () => {
+    renderWithRouterAndRedux(<App />, INITIAL_STATE, '/ranking');
+    const RANKING_TITLE = screen.getByTestId('ranking-title');
+    const BTN_HOME = screen.getByTestId('btn-go-home');
+    const PLAYERS_RANK = screen.getByTestId('player-name-0');
+    expect(RANKING_TITLE).toBeInTheDocument();
+    expect(BTN_HOME).toBeInTheDocument();
+    expect(PLAYERS_RANK).toBeInTheDocument();
   });
-  it('test if with a bad result change the text to Could Be Better!', () => {
-    renderWithRouterAndRedux(<App />, BAD_RESULT, '/feedback');
-    const SCORE_FEEDBACK = screen.getByTestId('feedback-text');
-    expect(SCORE_FEEDBACK.innerHTML).toBe('Could be better...');
-  })
-  it('test if the button play again works', () => {
-    const { history } = renderWithRouterAndRedux(<App />, BAD_RESULT, '/feedback');
-    const PLAY_AGAIN_BTN = screen.getByTestId('btn-play-again');
-    userEvent.click(PLAY_AGAIN_BTN);
-    const { location: { pathname } } = history;
-    expect(pathname).toEqual('/');
+  it('test if when click in Home, redirect the page to /', ()=>{
+    const { history }= renderWithRouterAndRedux(<App />, INITIAL_STATE, '/feedback');
+    const BTN_RANKING = screen.getByTestId('btn-ranking');
+    userEvent.click(BTN_RANKING);
   });
 });
